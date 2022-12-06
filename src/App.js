1a7a6f5919b624d7c6b0bab2f4cde9396d27c496
llvovll89 <svvvs5579@naver.com>
2022-12-06 21:23:53 +0900

    pages -> app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from "react";
-import { Pages } from "./components/pages/Pages";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from '../src/components/common/Header';
+import { Home } from '../src/components/home/Home';
+import { Services } from '../src/components/pages/Services/Services';
+import { Blog } from '../src/components/pages/Blog/Blog';
+import { Contact } from '../src/components/pages/Contact/Contact';
+import { Footer } from '../src/components/pages/Footer/Footer';
+import { Project } from '../src/components/pages/Project/Project';
 import Aos from 'aos';
-import 'aos/dist/aos.css'
+import 'aos/dist/aos.css';
 
 function App() {
   useEffect(() => {
     Aos.init({
-      duration : 1250
+      duration: 1250,
     });
-    Aos.refresh()
-
-  }, [])
+    Aos.refresh();
+  }, []);
 
   return (
     <>
-      <Pages />
+      <Router basename={process.env.PUBLIC_URL}>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from '../common/Header';
-import { Home } from '../home/Home';
-import { Services } from '../pages/Services/Services';
-import { Blog } from './Blog/Blog';
-import { Contact } from './Contact/Contact';
-import { Footer } from './Footer/Footer';
-import { Project } from './Project/Project';
+// import React from 'react';
 
-export const Pages = () => {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  );
-};
+
+// export const Pages = () => {
+//   return (
+//     <>
+
+//     </>
+//   );
+// };
